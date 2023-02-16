7fad756a6534e86c0fe6d94a6aa8d845a2deb479
barney <barney.shen@linkage-retail.com>
2023-02-16 21:54:09 +0800

    新增:mongodb

---
+++ src/main/java/com/barney/unionfly/domain/user/User.java
@@ -0,0 +1,20 @@
+package com.barney.unionfly.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private String password;
+}
