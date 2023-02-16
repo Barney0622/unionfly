16f44c673ba9d5c9694d67e22bef08dbe9999738
barney <barney.shen@linkage-retail.com>
2023-02-16 21:21:34 +0800

    init project

---
+++ src/main/java/com/barney/unionfly/UnionflyApplication.java
@@ -0,0 +1,13 @@
+package com.barney.unionfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnionflyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnionflyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/barney/unionfly/UnionflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barney.unionfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnionflyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
